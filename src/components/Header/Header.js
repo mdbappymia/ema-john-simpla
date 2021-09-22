f530f7934687d751a0de5e313ab9d4844847a406
mdbappymia <mbm.21.02.16@gmail.com>
2021-09-23 00:01:02 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+import logo from "../../images/logo.png";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src={logo} className="logo" alt="" />
+      <nav>
+        <a href="/shop">Shop</a>
+        <a href="/orders">Manage Order</a>
+        <a href="/inventory">Manage Inventory</a>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
